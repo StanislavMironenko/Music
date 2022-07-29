218c4f51ed0b61fdedb7b63b1503daa0a42791f0
StanislavMironenko <98009389+StanislavMironenko@users.noreply.github.com>
2022-07-29 16:27:50 +0300

    header start

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// function getEvenNumbers(start, end) {
+//   // Change code below this line
+//   let evenNumber = [];
+//   for (i = start; i <= end; i += 1) {
+//     if (i % 2 === 0) {
+//       evenNumber.push(i);
+//     }
+//     continue;
+//   }
+//   return evenNumber;
+//   // Change code above this line
+// }
+
+// console.log(getEvenNumbers(6, 12));
+// console.log(getEvenNumbers(8, 8)) 
+//  console.log(getEvenNumbers(3, 11));
+
+
+
